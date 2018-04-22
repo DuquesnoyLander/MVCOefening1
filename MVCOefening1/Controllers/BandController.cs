fa78a765b51aa8b4d2bcabcb55615f4702699e33
landerDuquesnoy <lander.duquesnoy@student.howest.be>
2018-04-22 18:30:43 +0200

    Done

---
+++ MVCOefening1/Controllers/BandController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyHobbies;
+
+namespace MVCOefening1.Controllers
+{
+    public class BandController : Controller
+    {
+        private List<Band> bands;
+        public BandController()
+        {
+            bands = BandInitializer.StartUp();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lijst()
+        {
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public ViewResult LijstMetLeden()
+        {
+            
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public ViewResult Maak(string naam, int? jaar)
+        {
+            Band NieuweBand = new Band() { Naam = naam ?? "Onbekend", Jaar = jaar ?? DateTime.Now.Year };
+            bands.Add(NieuweBand);
+            ViewBag.Bands = bands;
+
+            return View("Lijst");
+        }
+    }
+}
---
+++ MVCOefening1/Controllers/BandLidController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyHobbies;
+
+namespace MVCOefening1.Controllers
+{
+    public class BandLidController : Controller
+    {
+        private List<Band> bands;
+
+        public BandLidController()
+        {
+            bands = BandInitializer.StartUp();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lijst()
+        {
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public IActionResult Maak(string naam, Geslacht? geslacht, string band)
+        {
+            bands.Add(
+                new Band()
+                {
+                    Naam = band ?? "Onbekend",
+                    Leden = new List<BandLid>()
+                    {
+                        new BandLid()
+                        {
+                            Naam = naam ?? "Onbekend",
+                            Leeftijd = DateTime.Now.Year,
+                            Geslacht = geslacht ?? Geslacht.Man }
+                    }
+                });
+            ViewBag.Bands = bands;
+            return View("Lijst");
+        }
+    }
+}
